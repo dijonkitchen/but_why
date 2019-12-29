793ea879dd6cd98436a76b7a8e2a1b4952142f14
JC <11434205+dijonkitchen@users.noreply.github.com>
2019-12-29 14:22:54 -0800

    feat: add basic form

--- dijonkitchen.but-why/src/dijonkitchen/but_why/frontend/main.cljs
+++ dijonkitchen.but-why/src/dijonkitchen/but_why/frontend/main.cljs
@@ -1,14 +1,49 @@
-(ns ^:figwheel-hooks dijonkitchen.but-why.frontend.main)
+(ns ^:figwheel-hooks dijonkitchen.but-why.frontend.main
+  (:require [reagent.core :as r]))
 
-(js/console.log "Hello, world")
+(def reason
+  (r/atom nil))
+
+(defn- reason-handler
+  [event]
+  (reset! reason (-> event
+                     .-target
+                     .-value)))
+
+(def reasons
+  (r/atom []))
+
+(defn- submit-handler
+  [event]
+  (.preventDefault event)
+  (when-let [why @reason]
+    (swap! reasons conj why)
+    (reset! reason nil)))
+
+(defn- form
+  []
+  [:form
+   [:label {:for :reason-why}
+    "What do you want to do?"
+    [:br]
+    [:input {:id       :reason-why
+             :required true
+             :name     :reason
+             :value    @reason
+             :on-change reason-handler}]]
+   [:input {:type     :submit
+            :on-click submit-handler}]])
+
+(defn- root
+  []
+  [form])
 
 ;; This is called once
 (defonce init
-  (do (set! (.-innerHTML (js/document.getElementById "app"))
-            "<p>Loaded but-why!</p>
-            <p>Edit <strong><code>src/dijonkitchen/but_why/frontend/main.cljs</code></strong> to change this message.</p>")
-      true))
+  (r/render [root]
+            (js/document.getElementById "app")))
 
 ;; This is called every time you make a code change
 (defn ^:after-load reload []
-  (set! (.-innerText (js/document.getElementById "app")) "Hot Reloaded but-why!"))
+  (r/render [root]
+            (js/document.getElementById "app")))
