8a54015aad8ccfdb90fea4ed2e712c4641978980
JC <11434205+dijonkitchen@users.noreply.github.com>
2019-12-29 14:40:36 -0800

    feat: show previous answers

--- dijonkitchen.but-why/src/dijonkitchen/but_why/frontend/main.cljs
+++ dijonkitchen.but-why/src/dijonkitchen/but_why/frontend/main.cljs
@@ -1,42 +1,55 @@
 (ns ^:figwheel-hooks dijonkitchen.but-why.frontend.main
   (:require [reagent.core :as r]))
 
-(def reason
+(def answer
   (r/atom nil))
 
-(defn- reason-handler
+(defn- answer-handler
   [event]
-  (reset! reason (-> event
+  (reset! answer (-> event
                      .-target
                      .-value)))
 
-(def reasons
+(def answers
   (r/atom []))
 
 (defn- submit-handler
   [event]
   (.preventDefault event)
-  (when-let [why @reason]
-    (swap! reasons conj why)
-    (reset! reason nil)))
+  (when-let [response @answer]
+    (swap! answers conj response)
+    (reset! answer nil)))
 
 (defn- form
   []
   [:form
-   [:label {:for :reason-why}
+   [:label
     "What do you want to do?"
     [:br]
-    [:input {:id       :reason-why
-             :required true
-             :name     :reason
-             :value    @reason
-             :on-change reason-handler}]]
+    [:input {:required true
+             :auto-focus true
+             :value    @answer
+             :on-change answer-handler}]]
    [:input {:type     :submit
             :on-click submit-handler}]])
 
+(defn- previous-answer
+  [key answer]
+  [:p {:key key}
+   "What do you want to do?"
+   [:br]
+   answer])
+
+(defn- previous-answers
+  []
+  [:<>
+   (map-indexed previous-answer @answers)])
+
 (defn- root
   []
-  [form])
+  [:<>
+   [previous-answers]
+   [form]])
 
 ;; This is called once
 (defonce init
